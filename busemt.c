9e9dbd1aefe410cd7fdea21467c5f81b39e08775
divinity76 <divinity76@gmail.com>
2016-11-04 20:25:57 +0100

    force bool on likely/unlikely

--- busemt.c
+++ busemt.c
@@ -38,8 +38,8 @@
 
 #if !defined(likely)
 #if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
-#define likely(x)       __builtin_expect((x),1)
-#define unlikely(x)     __builtin_expect((x),0)
+#define likely(x)       __builtin_expect(!!(x),1)
+#define unlikely(x)     __builtin_expect(!!(x),0)
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
